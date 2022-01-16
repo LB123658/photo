e4a61472cec93d7c9fe70d8c315743925d9dbe25
LB123658 <79328251+LB123658@users.noreply.github.com>
2022-01-16 15:50:31 -0600

    Create update.js

---
+++ update.js
@@ -0,0 +1,7 @@
+var updated = "no";
+
+if (updated == "no") {
+  notify("You are using the downloaded version and an update is available. Download it here: <br><a href='https://lb123658.github.io/photo/start' target='_blank'></a>");
+} else {
+  console.log("Up to date");
+}
